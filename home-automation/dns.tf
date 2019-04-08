807f190d8aae350dab7ab6bf2b10a8ac372d0544
Trey Thomas <trey@treythomas.me>
2019-04-07 21:40:46 -0600

    Add alias record home.treythomas.me -> public.home.treythomas.me

--- home-automation/dns.tf
+++ home-automation/dns.tf
@@ -1,3 +1,15 @@
 resource "aws_route53_zone" "home" {
   name = "home.${var.parent_domain}."
 }
+
+resource "aws_route53_record" "home_public_alias" {
+  zone_id = "${aws_route53_zone.home.zone_id}"
+  name = "home.treythomas.me."
+  type = "A"
+
+  alias {
+    name = "public.home.treythomas.me."
+    zone_id = "${aws_route53_zone.home.zone_id}"
+    evaluate_target_health = false
+  }
+}
